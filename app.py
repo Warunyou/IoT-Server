9ab732f7bfff9dc6afbb7f6318e8998515c701cc
Warunyou <warunyou.top@gmail.com>
2018-05-20 13:43:07 +0700

    First app.py ready to run

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-RAM
-testtt
+from flask import Flask, request, abort, make_response
+from flask_httpauth import HTTPBasicAuth
+
+import requests, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return 'OK'
+
+if __name__ == "__main__":
+    app.run()
