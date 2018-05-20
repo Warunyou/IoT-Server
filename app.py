95ad06eb6a682cc969bc3d9abe009f4f8ba8ccac
Warunyou <warunyou.top@gmail.com>
2018-05-20 14:13:23 +0700

    Test post methods

--- app.py
+++ app.py
@@ -14,5 +14,18 @@ def hello():
 	username = request.args.get('name')
 	return 'hello ' + username
 
+@app.route('/callback', methods=['POST'])
+def callback():
+	payload = request.get_json()
+	jsonstring = json.dumps(payload)
+	data_dict = json.loads(jsonstring)
+	user_name = data_dict['name']
+	user_id = data_dict['id']
+	result = json_string + '\n'
+	result = result + user_name +' '+user_id
+	
+	return result
+
+
 if __name__ == "__main__":
     app.run()
